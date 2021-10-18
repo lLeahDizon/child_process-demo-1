e2c219b3d2f1c5b23a12b133e713968a9d5059db
lLeahDizon <516330797@qq.com>
2021-10-18 20:37:37 +0800

    feature: execFile options 常用属性

--- 1.js
+++ 1.js
@@ -3,8 +3,12 @@ const {execFile} = child_process
 
 const userInput = '.'
 
-const streams = execFile('ls', ['-la', userInput], {}, () => {})
-
-streams.stdout.on('data', (chuck) => {
-  console.log(chuck)
+const streams = execFile('ls', ['-la', userInput], {
+  cwd: '', // 设置运行目录
+  env: {NODE_ENV: 'development'},  // 设置 node 运行的环境
+  shell: '', // 配置使用的命令
+  maxBuffer: 1024 * 1024, // 最大内存
+}, (error, stdout) => {
+  console.log(error)
+  console.log(stdout)
 })
