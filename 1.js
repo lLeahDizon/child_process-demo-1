e55a419b216a4aa376c924951f16c5811a87a637
lLeahDizon <516330797@qq.com>
2021-10-19 19:38:34 +0800

    feature: 使用 spawn

--- 1.js
+++ 1.js
@@ -1,14 +1,13 @@
 const child_process = require('child_process')
-const {execFile} = child_process
+const {spawn} = child_process
 
 const userInput = '.'
 
-const streams = execFile('ls', ['-la', userInput], {
-  cwd: '', // 设置运行目录
-  env: {NODE_ENV: 'development'},  // 设置 node 运行的环境
-  shell: '', // 配置使用的命令
-  maxBuffer: 1024 * 1024, // 最大内存
-}, (error, stdout) => {
-  console.log(error)
-  console.log(stdout)
+const streams = spawn('ls', ['-la', userInput], {
+  cwd: '/users/apple',
+  env: {NODE_ENV: 'development'}
+})
+
+streams.stdout.on('data', (chunk) => {
+  console.log(chunk.toString())
 })
